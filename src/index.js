efcfd70f16bbf054d9a23375be29f27a28571a5f
Ryan Efendy <ryanefendy95@gmail.com>
2019-01-22 07:30:20 -0800

    FEAT: add redux-devtools

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import reducer from './reducers';
+import middleware from './middleware';
 
-const store = createStore(reducer);
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancer(middleware),
+
+);
 
 ReactDOM.render(
   <Provider store={store}>
