dbd3a8dcdc225dad5a75a31a72569e8c0acbba73
Ryan Efendy <ryanefendy95@gmail.com>
2019-01-27 17:01:49 -0800

    FEAT: sort leaderboard

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Item, Grid, Container, Rating, Label } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 
-const LeaderBoard = () => (
-  <Container text>
+const LeaderBoard = ({users}) => users.map((user, i) => (
+  <Container text key={user.id}>
     <Grid centered celled>
       <Grid.Row>
         <Grid.Column width={12}>
@@ -10,86 +11,139 @@ const LeaderBoard = () => (
             <Item>
               <Item.Image
                 size="tiny"
-                src="https://react.semantic-ui.com/images/avatar/large/jenny.jpg"
+                src={user.avatarURL}
               />
               <Item.Content>
-                <Item.Header as="a">Sarah Edo</Item.Header>
+                <Item.Header as="a">{user.name}</Item.Header>
                 <Rating
                   maxRating={5}
-                  defaultRating={5}
+                  defaultRating={5 - i}
                   icon="star"
                   style={{ paddingLeft: 10 }}
                 />
-                <Item.Description>Answered Questions: 7</Item.Description>
-                <Item.Description>Created Questions: 3</Item.Description>
+                <Item.Description>{`Answered Questions: ${Object.keys(user.answers).length}`}</Item.Description>
+                <Item.Description>{`Created Questions: ${user.questions.length}`}</Item.Description>
               </Item.Content>
             </Item>
           </Item.Group>
         </Grid.Column>
         <Grid.Column width={4} textAlign="center" verticalAlign="middle">
           <Label circular color="teal" size="massive">
-            10
+            {Object.keys(user.answers).length + user.questions.length}
           </Label>
         </Grid.Column>
-
-        <Grid.Column width={12}>
-          <Item.Group>
-            <Item>
-              <Item.Image
-                size="tiny"
-                src="https://react.semantic-ui.com/images/avatar/large/justen.jpg"
-              />
-
-              <Item.Content>
-                <Item.Header as="a">Tyler McGinnis</Item.Header>
-                <Rating
-                  maxRating={5}
-                  defaultRating={4}
-                  icon="star"
-                  style={{ paddingLeft: 10 }}
-                />
-                <Item.Description>Answered Questions: 7</Item.Description>
-                <Item.Description>Created Questions: 3</Item.Description>
-              </Item.Content>
-            </Item>
-          </Item.Group>
-        </Grid.Column>
-        <Grid.Column width={4} textAlign="center" verticalAlign="middle">
-          <Label circular color="teal" size="massive">
-            10
-          </Label>
-        </Grid.Column>
-
-        <Grid.Column width={12}>
-          <Item.Group>
-            <Item>
-              <Item.Image
-                size="tiny"
-                src="https://react.semantic-ui.com/images/avatar/large/veronika.jpg"
-              />
-
-              <Item.Content>
-                <Item.Header as="a">John Doe</Item.Header>
-                <Rating
-                  maxRating={5}
-                  defaultRating={3}
-                  icon="star"
-                  style={{ paddingLeft: 10 }}
-                />
-                <Item.Description>Answered Questions: 7</Item.Description>
-                <Item.Description>Created Questions: 3</Item.Description>
-              </Item.Content>
-            </Item>
-          </Item.Group>
-        </Grid.Column>
-        <Grid.Column width={4} textAlign="center" verticalAlign="middle">
-          <Label circular color="teal" size="massive">
-            10
-          </Label>
-        </Grid.Column>
-      </Grid.Row>
+        </Grid.Row>
     </Grid>
   </Container>
-);
+))
 
-export default LeaderBoard;
+const compare = (a, b) => {
+  if (
+    Object.keys(a.answers).length + a.questions.length >
+    Object.keys(b.answers).length + b.questions.length
+  ) {
+    return -1;
+  }
+  if (
+    Object.keys(a.answers).length + a.questions.length <
+    Object.keys(b.answers).length + b.questions.length
+  ) {
+    return 1;
+  }
+  return 0;
+};
+
+const mapStateToProps = ({ users }) => ({users: Object.values(users).sort(compare)});
+
+export default connect(mapStateToProps)(LeaderBoard);
+
+// (
+//   <Container text>
+//     <Grid centered celled>
+//       <Grid.Row>
+//         <Grid.Column width={12}>
+//           <Item.Group>
+//             <Item>
+//               <Item.Image
+//                 size="tiny"
+//                 src="https://react.semantic-ui.com/images/avatar/large/jenny.jpg"
+//               />
+//               <Item.Content>
+//                 <Item.Header as="a">Sarah Edo</Item.Header>
+//                 <Rating
+//                   maxRating={5}
+//                   defaultRating={5}
+//                   icon="star"
+//                   style={{ paddingLeft: 10 }}
+//                 />
+//                 <Item.Description>Answered Questions: 7</Item.Description>
+//                 <Item.Description>Created Questions: 3</Item.Description>
+//               </Item.Content>
+//             </Item>
+//           </Item.Group>
+//         </Grid.Column>
+//         <Grid.Column width={4} textAlign="center" verticalAlign="middle">
+//           <Label circular color="teal" size="massive">
+//             10
+//           </Label>
+//         </Grid.Column>
+
+//         <Grid.Column width={12}>
+//           <Item.Group>
+//             <Item>
+//               <Item.Image
+//                 size="tiny"
+//                 src="https://react.semantic-ui.com/images/avatar/large/justen.jpg"
+//               />
+
+//               <Item.Content>
+//                 <Item.Header as="a">Tyler McGinnis</Item.Header>
+//                 <Rating
+//                   maxRating={5}
+//                   defaultRating={4}
+//                   icon="star"
+//                   style={{ paddingLeft: 10 }}
+//                 />
+//                 <Item.Description>Answered Questions: 7</Item.Description>
+//                 <Item.Description>Created Questions: 3</Item.Description>
+//               </Item.Content>
+//             </Item>
+//           </Item.Group>
+//         </Grid.Column>
+//         <Grid.Column width={4} textAlign="center" verticalAlign="middle">
+//           <Label circular color="teal" size="massive">
+//             10
+//           </Label>
+//         </Grid.Column>
+
+//         <Grid.Column width={12}>
+//           <Item.Group>
+//             <Item>
+//               <Item.Image
+//                 size="tiny"
+//                 src="https://react.semantic-ui.com/images/avatar/large/veronika.jpg"
+//               />
+
+//               <Item.Content>
+//                 <Item.Header as="a">John Doe</Item.Header>
+//                 <Rating
+//                   maxRating={5}
+//                   defaultRating={3}
+//                   icon="star"
+//                   style={{ paddingLeft: 10 }}
+//                 />
+//                 <Item.Description>Answered Questions: 7</Item.Description>
+//                 <Item.Description>Created Questions: 3</Item.Description>
+//               </Item.Content>
+//             </Item>
+//           </Item.Group>
+//         </Grid.Column>
+//         <Grid.Column width={4} textAlign="center" verticalAlign="middle">
+//           <Label circular color="teal" size="massive">
+//             10
+//           </Label>
+//         </Grid.Column>
+//       </Grid.Row>
+//     </Grid>
+//   </Container>
+// );
--- src/components/Poll.js
+++ src/components/Poll.js
@@ -8,7 +8,6 @@ import {
   Progress,
   Icon
 } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { handdleAnswerQuestion } from '../actions/questions';
 
